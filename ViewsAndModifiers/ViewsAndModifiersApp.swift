2dcc2b3a47ca4138f1a035c796652d0a12adbe87
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-07-21 14:12:25 +0530

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Shubham on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
